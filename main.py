f14acfe8880d1487f2496229067c8cdf51e69270
sinofarmonov323 <farmonovsino4@gmail.com>
2025-07-12 18:50:38 +0500

    updated

--- main.py
+++ main.py
@@ -16,27 +16,27 @@ async def start(message: types.Message):
     add_user(message.from_user.id, message.from_user.username)
     await message.answer(f"Salom {html.bold(message.from_user.full_name)}\nbu botda siz har xil APIlar olshingiz mumkin\nSizga qanday API kerak", reply_markup=apis_button(get_all_apis()))
 
-# @dp.message(Command("admin"))
-# async def admin_panel(message: types.Message):
-#     if message.from_user.id == ADMIN_ID:
-#         await message.answer(f"Admin panel", reply_markup=admin_panel_button())
-#     else:
-#         await message.answer("/admin")
+@dp.message(Command("admin"))
+async def admin_panel(message: types.Message):
+    if message.from_user.id == ADMIN_ID:
+        await message.answer(f"Admin panel", reply_markup=admin_panel_button())
+    else:
+        await message.answer("/admin")
 
 @dp.message(Command("users"))
 async def send_users_number(message: types.Message):
     await message.answer(f"Foydalanuvchilar soni: {len(get_all_users_number())}")
 
-# @dp.callback_query(F.data.in_(["api", "see_users"]))
-# async def send_users_number(call: types.CallbackQuery):
-#     if call.from_user.id == ADMIN_ID:
-#         if call.data == "see_users":
-#             await call.message.answer(f"Foydalanuvchilar soni: {len(get_all_users_number())}")
-#         elif call.data == "api":
-#             await call.message.answer(f"Ishlatish: {html.code("/add_api")} api_name, api_url, callback_name, sarlavha, narx")
-#         await call.answer()
-#     else:
-#         print("NOT EQUAL")
+@dp.callback_query(F.data.in_(["api", "see_users"]))
+async def send_users_number(call: types.CallbackQuery):
+    if call.from_user.id == ADMIN_ID:
+        if call.data == "see_users":
+            await call.message.answer(f"Foydalanuvchilar soni: {len(get_all_users_number())}")
+        elif call.data == "api":
+            await call.message.answer(f"Ishlatish: {html.code("/add_api")} api_name, api_url, callback_name, sarlavha, narx")
+        await call.answer()
+    else:
+        print("NOT EQUAL")
     
 @dp.message(Command("delete"))
 async def delete_the_api(message: types.Message, command: CommandObject):
